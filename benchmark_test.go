966e24fb05eb6ae77232e9ab1ef935b65d814126
Dwi Siswanto <git@dw1.io>
2025-02-04 09:16:52 +0700

    test: add mapsutil
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,16 +1,22 @@
 package swiss_table
 
 import (
+	"runtime"
 	"strconv"
 	"testing"
 
 	cockroachSwiss "github.com/cockroachdb/swiss"
 	dolthubSwiss "github.com/dolthub/swiss"
 	csmap "github.com/mhmtszr/concurrent-swiss-map"
+	mapsutil "github.com/projectdiscovery/utils/maps"
 )
 
+func init() {
+	runtime.GOMAXPROCS(1)
+}
+
 // keyCount defines the number of keys to use in benchmarks.
-var keyCounts = []int{100_000, 500_000, 1_000_000, 2_500_000, 5_000_000, 10_000_000}
+var keyCounts = []int{100, 1_000, 10_000, 100_000, 500_000, 1_000_000, 2_500_000, 5_000_000, 10_000_000}
 
 // generateKeys returns a slice of keyCount strings: "key0", "key1", ….
 func generateKeys(n int) []string {
@@ -26,7 +32,10 @@ func BenchmarkPut(b *testing.B) {
 		b.Run(strconv.Itoa(keyCount), func(b *testing.B) {
 			keys := generateKeys(keyCount)
 			mapStd := make(map[string]int)
-			mapCsmap := csmap.Create[string, int]()
+			mapMapsutil := make(mapsutil.Map[string, int])
+			mapOrderedMap := mapsutil.NewOrderedMap[string, int]()
+			mapSyncLockMap := mapsutil.NewSyncLockMap[string, int]()
+			mapCsmap := csmap.Create(csmap.WithSize[string, int](uint64(keyCount)))
 			mapCockroachDB := cockroachSwiss.New[string, int](keyCount)
 			mapDolthub := dolthubSwiss.NewMap[string, int](uint32(keyCount))
 
@@ -35,6 +44,33 @@ func BenchmarkPut(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					for j, key := range keys {
 						mapStd[key] = j
+					}
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					for j, key := range keys {
+						mapMapsutil.Set(key, j)
+					}
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil-OrderedMap", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					for j, key := range keys {
+						mapOrderedMap.Set(key, j)
+					}
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil-SyncLockMap", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					for j, key := range keys {
+						mapSyncLockMap.Set(key, j)
 					}
 				}
 			})
@@ -74,12 +110,18 @@ func BenchmarkGet(b *testing.B) {
 		b.Run(strconv.Itoa(keyCount), func(b *testing.B) {
 			keys := generateKeys(keyCount)
 			mapStd := make(map[string]int)
-			mapCsmap := csmap.Create[string, int]()
+			mapMapsutil := make(mapsutil.Map[string, int])
+			mapOrderedMap := mapsutil.NewOrderedMap[string, int]()
+			mapSyncLockMap := mapsutil.NewSyncLockMap[string, int]()
+			mapCsmap := csmap.Create(csmap.WithSize[string, int](uint64(keyCount)))
 			mapCockroachDB := cockroachSwiss.New[string, int](keyCount)
 			mapDolthub := dolthubSwiss.NewMap[string, int](uint32(keyCount))
 
 			for j, key := range keys {
 				mapStd[key] = j
+				mapMapsutil.Set(key, j)
+				mapOrderedMap.Set(key, j)
+				mapSyncLockMap.Set(key, j)
 				mapCsmap.Store(key, j)
 				mapCockroachDB.Put(key, j)
 				mapDolthub.Put(key, j)
@@ -91,6 +133,33 @@ func BenchmarkGet(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					key := keys[i%nKeys]
 					_, _ = mapStd[key]
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil", func(b *testing.B) {
+				nKeys := len(keys)
+				for i := 0; i < b.N; i++ {
+					key := keys[i%nKeys]
+					_, _ = mapMapsutil.Get(key)
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil-OrderedMap", func(b *testing.B) {
+				nKeys := len(keys)
+				for i := 0; i < b.N; i++ {
+					key := keys[i%nKeys]
+					_, _ = mapOrderedMap.Get(key)
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil-SyncLockMap", func(b *testing.B) {
+				nKeys := len(keys)
+				for i := 0; i < b.N; i++ {
+					key := keys[i%nKeys]
+					_, _ = mapSyncLockMap.Get(key)
 				}
 			})
 
@@ -129,17 +198,21 @@ func BenchmarkDelete(b *testing.B) {
 		b.Run(strconv.Itoa(keyCount), func(b *testing.B) {
 			keys := generateKeys(keyCount)
 			mapStd := make(map[string]int)
-			mapCsmap := csmap.Create[string, int]()
+			mapMapsutil := make(mapsutil.Map[string, int])
+			mapOrderedMap := mapsutil.NewOrderedMap[string, int]()
+			mapSyncLockMap := mapsutil.NewSyncLockMap[string, int]()
+			mapCsmap := csmap.Create(csmap.WithSize[string, int](uint64(keyCount)))
 			mapCockroachDB := cockroachSwiss.New[string, int](keyCount)
 			mapDolthub := dolthubSwiss.NewMap[string, int](uint32(keyCount))
 
-			for i := 0; i < b.N; i++ {
-				for j, key := range keys {
-					mapStd[key] = j
-					mapCsmap.Store(key, j)
-					mapCockroachDB.Put(key, j)
-					mapDolthub.Put(key, j)
-				}
+			for j, key := range keys {
+				mapStd[key] = j
+				mapMapsutil.Set(key, j)
+				mapOrderedMap.Set(key, j)
+				mapSyncLockMap.Set(key, j)
+				mapCsmap.Store(key, j)
+				mapCockroachDB.Put(key, j)
+				mapDolthub.Put(key, j)
 			}
 
 			b.ResetTimer()
@@ -148,6 +221,31 @@ func BenchmarkDelete(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					key := keys[i%nKeys]
 					delete(mapStd, key)
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil/no-op", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					// no-op: mapsutil.Map does not have a Delete method
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil-OrderedMap", func(b *testing.B) {
+				nKeys := len(keys)
+				for i := 0; i < b.N; i++ {
+					key := keys[i%nKeys]
+					mapOrderedMap.Delete(key)
+				}
+			})
+
+			b.ResetTimer()
+			b.Run("mapsutil-SyncLockMap", func(b *testing.B) {
+				nKeys := len(keys)
+				for i := 0; i < b.N; i++ {
+					key := keys[i%nKeys]
+					mapSyncLockMap.Delete(key)
 				}
 			})
 
--- go.mod
+++ go.mod
@@ -7,4 +7,16 @@ require (
 	github.com/dolthub/maphash v0.1.0 // indirect
 	github.com/dolthub/swiss v0.2.1 // indirect
 	github.com/mhmtszr/concurrent-swiss-map v1.0.8 // indirect
+	github.com/miekg/dns v1.1.56 // indirect
+	github.com/projectdiscovery/blackrock v0.0.1 // indirect
+	github.com/projectdiscovery/utils v0.4.9 // indirect
+	github.com/tidwall/gjson v1.14.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 )
